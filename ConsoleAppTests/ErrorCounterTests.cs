2df883e948273aab58b7488dcb6dbc0ae111063e
Andrew <andrewboudreau@gmail.com>
2017-03-23 23:16:48 -0500

    More test for error counting

--- ConsoleAppTests/ErrorCounterTests.cs
+++ ConsoleAppTests/ErrorCounterTests.cs
@@ -12,22 +12,78 @@ namespace ConsoleAppTests
     public class ErrorCounterTests
     {
         [TestMethod]
-        public void NoErrors()
+        public void Expect72ErrorsWithCountedBlocks()
         {
             var matrix = Program.CreateMatrix();
-            MatrixWith18Errors(matrix);
+            CountedBlocks(matrix);
 
             var errors = Program.Error(matrix);
             Assert.AreEqual(72, errors);
         }
 
-        public static void MatrixWith18Errors(int[][] matrix)
+        [TestMethod]
+        public void Expect144ErrorsWithAllOnes()
         {
-            for(var i = 0; i < matrix.Length; i++)
+            var matrix = Program.CreateMatrix();
+            AllToGivenValue(matrix, 1);
+
+            var errors = Program.Error(matrix);
+            Assert.AreEqual(144, errors);
+        }
+
+        [TestMethod]
+        public void NoErrors()
+        {
+            var errors = Program.Error(Problems.NonEasyProblem_Solution);
+            Assert.AreEqual(0, errors);
+        }
+
+        [TestMethod]
+        public void TwoErrors()
+        {
+            // Arrange
+            var problem = Program.CopyMatrix(Problems.NonEasyProblem_Solution);
+            problem[0][0] = 1;
+
+            // Act
+            var errors = Program.Error(problem);
+
+            // Assert
+            Assert.AreEqual(2, errors);
+        }
+
+        [TestMethod]
+        public void AllValuesMissing()
+        {
+            // Arrange
+            var matrix = Program.CreateMatrix();
+            AllToGivenValue(matrix, 0);
+
+            // Act
+            var errors = Program.Error(matrix);
+
+            // Assert all rows and all columns are in errors
+            Assert.AreEqual((9 * 9) + (9 * 9), errors);
+        }
+
+        public static void CountedBlocks(int[][] matrix)
+        {
+            for (var i = 0; i < matrix.Length; i++)
             {
-                for(var k = 0; k < matrix[i].Length; k++)
+                for (var k = 0; k < matrix[i].Length; k++)
                 {
-                    matrix[i][k] = k+1;
+                    matrix[i][k] = k + 1;
+                }
+            }
+        }
+
+        public static void AllToGivenValue(int[][] matrix, int value)
+        {
+            for (var i = 0; i < matrix.Length; i++)
+            {
+                for (var k = 0; k < matrix[i].Length; k++)
+                {
+                    matrix[i][k] = value;
                 }
             }
         }
---
+++ ConsoleAppTests/Problems.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppTests
+{
+    public class Problems
+    {
+        public static int[][] NonEasyProblem = new int[][]
+        {
+            new int[] { 0,0,6, 2,0,0, 0,8,0 },
+            new int[] { 0,0,8, 9,7,0, 0,0,0 },
+            new int[] { 0,0,4, 8,1,0, 5,0,0 },
+
+            new int[] { 0,0,0, 0,6,0, 0,0,2 },
+            new int[] { 0,7,0, 0,0,0, 0,3,0 },
+            new int[] { 6,0,0, 0,5,0, 0,0,0 },
+
+            new int[] { 0,0,2, 0,4,7, 1,0,0 },
+            new int[] { 0,0,3, 0,2,8, 4,0,0 },
+            new int[] { 0,5,0, 0,0,1, 2,0,0 }
+        };
+
+        public static int[][] NonEasyProblem_Solution = new int[][]
+        {
+            new int[] { 7,1,6, 2,3,5, 9,8,4 },
+            new int[] { 5,2,8, 9,7,4, 3,1,6 },
+            new int[] { 3,9,4, 8,1,6, 5,2,7 },
+
+            new int[] { 8,4,5, 1,6,3, 7,9,2 },
+            new int[] { 2,7,1, 4,8,9, 6,3,5 },
+            new int[] { 6,3,9, 7,5,2, 8,4,1 },
+
+            new int[] { 9,8,2, 6,4,7, 1,5,3 },
+            new int[] { 1,6,3, 5,2,8, 4,7,9 },
+            new int[] { 4,5,7, 3,9,1, 2,6,8 }
+        };
+    }
+}
